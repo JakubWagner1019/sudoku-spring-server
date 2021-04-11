25fe30f0e7472e4dba02e8562e4d1daadfec0b22
jakwagne <jakwagne@cisco.com>
2021-04-11 14:19:47 +0200

    first commit

---
+++ src/test/java/qb/sudoku/sudoku/SudokuApplicationTests.java
@@ -0,0 +1,13 @@
+package qb.sudoku.sudoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
