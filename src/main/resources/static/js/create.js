4eb461e7c254ec3ad851287faa0e2dec72a3abd0
jakwagne <jakwagne@cisco.com>
2021-04-24 23:39:48 +0200

    issue #10 - selected cell now has different background color

--- src/main/resources/static/js/create.js
+++ src/main/resources/static/js/create.js
@@ -3,6 +3,9 @@ const digitSelectionCells = document.getElementsByClassName("digit-selection-cel
 const sudokuGridCells = document.getElementsByClassName("sudoku-cell");
 const removeButton = document.getElementById("remove-button");
 
+const selectedClassName = "selected-sudoku-cell";
+const unselectedClassName = "unselected-sudoku-cell";
+
 let selected = null;
 let given = true;
 
@@ -26,13 +29,22 @@ function clearSelected() {
         "<input type='hidden' name='given[]'/>"
 }
 
+function chooseNewSelected(cell){
+    //clear previous selected
+    if (selected != null) {
+        selected.classList.toggle(selectedClassName);
+    }
+    //make new selected
+    selected = cell;
+    selected.classList.toggle(selectedClassName);
+}
+
 function main(){
     modifiable.innerHTML = "Modified"
 
     for (let i = 0; i < digitSelectionCells.length; i++) {
         let cell = digitSelectionCells[i];
         cell.addEventListener("click", function () {
-            console.log(this.textContent);
             setSelectedValue(this.textContent);
         })
     }
@@ -40,8 +52,7 @@ function main(){
     for (let i = 0; i < sudokuGridCells.length; i++) {
         let cell = sudokuGridCells[i];
         cell.addEventListener("click", function () {
-            selected = this;
-            console.log(selected.childNodes);
+            chooseNewSelected(this);
         })
     }
 
